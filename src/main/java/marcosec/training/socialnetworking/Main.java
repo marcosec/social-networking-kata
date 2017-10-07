4185d576c72549956ec1ee52fccfca46e8b45ed5
m.secchiero <m.secchiero@reply.it>
2017-10-07 12:01:46 +0200

    changed exit command

--- src/main/java/marcosec/training/socialnetworking/Main.java
+++ src/main/java/marcosec/training/socialnetworking/Main.java
@@ -21,7 +21,7 @@ public class Main
 {
 
     public static final String PROMPT_COMMAND = "> ";
-    public static final String EXIT_COMMAND = "0";
+    public static final String EXIT_COMMAND = "exit";
 
     public static void main(String[] args)
     {
